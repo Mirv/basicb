ae4c2f7457df5dde8252336937eed2a681ad6119
Mirv <reski.raptor@gmail.com>
2017-04-12 16:06:57 +0000

    adjusted seeding somewhat

--- basicB/db/seeds/020_dashes.rb
+++ basicB/db/seeds/020_dashes.rb
@@ -1,7 +1,7 @@
 def userDash(a_user)
   new_name = Faker::Cat.name
-  new_name = "#{a_user["name"]}'s cat buddy ... #{new_name}"
-  user_dash = user.dashes.create!(name: new_name)
+  new_name = "#{a_user.name}'s cat buddy ... #{new_name}"
+  user_dash = a_user.dashes.create!(name: new_name)
   return user_dash
 end
 
