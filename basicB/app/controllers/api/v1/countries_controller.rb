707380a415dc7da5c17dfa43b2153662ec4f52dd
Mirv <reski.raptor@gmail.com>
2018-10-08 21:45:07 +0000

    countries& players added to api

--- basicB/app/controllers/api/v1/countries_controller.rb
+++ basicB/app/controllers/api/v1/countries_controller.rb
@@ -4,4 +4,17 @@ class Api::V1::CountriesController < Api::V1::BaseController
 
     render(json: Api::V1::CountrySerializer.new(country).to_json)
   end
+  
+  def index
+    countries = Country.all
+
+    render(
+      json: ActiveModel::Serializer::CollectionSerializer.new(
+        countries,
+        # each_serializer: Api::V1::CountrySerializer,
+        serializer: Api::V1::CountrySerializer,
+        root: 'countries',
+      )
+    )
+  end
 end
--- basicB/app/controllers/api/v1/players_controller.rb
+++ basicB/app/controllers/api/v1/players_controller.rb
@@ -4,4 +4,17 @@ class Api::V1::PlayersController < Api::V1::BaseController
 
     render(json: Api::V1::PlayerSerializer.new(player).to_json)
   end
+  
+    def index
+    players = Player.all
+    render(
+      json: ActiveModel::Serializer::CollectionSerializer.new(
+        players,
+        # each_serializer: Api::V1::UserSerializer,
+        # each_serializer: UserSerializer,
+        # root: 'users',
+        serializer: PlayerSerializer,
+      )
+    )
+  end
 end
